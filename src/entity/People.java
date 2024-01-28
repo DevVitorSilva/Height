6d4201f7eda8b1881e6a8ee0b32c8ea907bb250d
DevVitorSilva <vitor.silva001080@gmail.com>
2024-01-28 14:24:54 -0300

    create a getters and setters

--- src/entity/People.java
+++ src/entity/People.java
@@ -1,4 +1,38 @@
 package entity;
 
 public class People {
+    private String name;
+    private double height;
+    private int age;
+
+    public People(String name, double height, int age){
+        this.age = age;
+        this.height = height;
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public double getHeight(){
+        return this.height;
+    }
+
+    public void setHeight(double height){
+        this.height = height;
+    }
+
 }
